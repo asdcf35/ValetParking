f4a3dc4c98dfb9ef4a11ece3db686a65b553dfce
asdcf35 <66645620+asdcf35@users.noreply.github.com>
2021-10-09 19:16:57 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
 //Create a reference for canvas 
-
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d")
 //Give specific height and width to the car image
-
+const greencar_width = 75;
+const greencar_height = 100;
 background_image = "parkingLot.jpg";
 greencar_image = "car2.png";
 
 //Set initial position for a car image.
+var greencar_x = 5;
+var greencar_y = 225;
 
 function add() {
 	//upload car, and background images on the canvas.
+	background_imgTag = new Image();
+	background_imgTag.onload = uploadBackground;
+	background_imgTag.src = background_image;
+
+	greencar_imgTag = new Image();
+	greencar_imgTag.onload = uploadBackground;
+	greencar_imgTag.src = background_image;
 }
 
 function uploadBackground() {
 	//Define function ‘uploadBackground’
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
 
 }
 
 function uploadgreencar() {
 	//Define function ‘uploadgreencar’.
+	ctx.drawImage(background_imgTag, greencar_x, greencar_y, greencar_width, greencar_height);
 
-	
 }
-
-
 window.addEventListener("keydown", my_keydown);
 
 function my_keydown(e)
@@ -55,23 +65,44 @@ function my_keydown(e)
 		
 		
 }
-
 function up()
 {
-	//Define function to move the car upward
+	if(rover_y >=0)
+	{
+		rover_y = rover_y - 10;
+		console.log("When up arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		 uploadBackground();
+		 uploadrover();
+	}
 }
-
 function down()
 {
-	//Define function to move the car downward
+	if(rover_y <=500)
+	{
+		rover_y =rover_y+ 10;
+		console.log("When down arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		 uploadrover();
+	}
 }
-
 function left()
 {
-	//Define function to move the car left side
+	if(rover_x >= 0)
+	{
+		rover_x =rover_x - 10;
+		console.log("When left arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		 uploadrover();
+	}
 }
-
 function right()
 {
-	//Define function to move the car right side
+	if(rover_x <= 700)
+	{
+		rover_x =rover_x + 10;
+		console.log("When right arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		uploadrover();
+   }
 }
+	
